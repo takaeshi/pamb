cf0ed5587e3e5be7e69a64a90a6827e6ac0e715a
Gwendolyn <53878781+Gwenstephanie@users.noreply.github.com>
2020-07-26 17:23:37 +0100

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -94,10 +94,10 @@ def get_readable_message():
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
                 msg += f"\n\n<b>Progress :</b> <code>{get_progress_bar_string(download)}</code> <b>{download.progress()}</b> \n\n<b>Size :</b> " \
                        f"{download.size()}" \
-                       f" \n<b>Speed :</b> {download.speed()} \n<b>Time Left :</b> {download.eta()} "\n
+                       f" \n<b>Speed :</b> {download.speed()} \n<b>Time Left :</b> {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"Peers: {download.aria_download().connections} " >
+                    msg += f"\nPeers: {download.aria_download().connections} " >
                            f"Seeders: {download.aria_download().num_seeders}"
                 msg += f"\n<b>Cancel 🚫 :</b> <code>{download.gid()}</code>"
             msg += "\n\n"
