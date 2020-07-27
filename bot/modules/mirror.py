e2965b374331c3118510d6b4e65bee5eac1a03dc
Gwendolyn <53878781+Gwenstephanie@users.noreply.github.com>
2020-07-27 11:45:21 +0100

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -176,7 +176,7 @@ def _mirror(bot, update, isTar=False):
                 else:
                     link = file.get_file().file_path
     else:
-        tag = self.message.from_user.username
+        tag = reply_to.from_user.username
     if not bot_utils.is_url(link) and not bot_utils.is_magnet(link):
         sendMessage('No download source provided', bot, update)
         return
