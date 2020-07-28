6bb9f31c6fa65602f2f4c7fa5a375b09f42e2bbb
Gwendolyn <53878781+Gwenstephanie@users.noreply.github.com>
2020-07-28 13:02:54 +0100

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -89,7 +89,7 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>○ File :</b> <code>{download.name()}</code> \n<b>○ Size :</b> {download.size()} \n<b>Status :</b> "
+            msg += f"<b>○ File :</b> <code>{download.name()}</code> \n<b>○ Size :</b> {download.size()} \n<b>○ Status :</b> "
             msg += download.status()
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
                 msg += f"\n<code>{get_progress_bar_string(download)}</code> <code><b>{download.progress()}</b></code>" \
