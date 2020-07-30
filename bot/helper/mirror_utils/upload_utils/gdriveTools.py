843798f0244f80ae8b04c72bfaf200c01eb4b040
Gwendolyn <53878781+Gwenstephanie@users.noreply.github.com>
2020-07-30 19:36:16 +0100

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -409,16 +409,16 @@ class GoogleDriveHelper:
         for file in response.get('files', []):
             if file.get(
                     'mimeType') == "application/vnd.google-apps.folder":  # Detect Whether Current Entity is a Folder or File.
-                msg += f"⁍ <a href='https://drive.google.com/drive/folders/{file.get('id')}'>{file.get('name')}" \
-                       f"</a> (folder)"
+                msg += f"⁍ https://drive.google.com/drive/folders/{file.get('id')}" \
+                       f"(folder)"
                 if INDEX_URL is not None:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}/')
-                    msg += f' | <a href="{url}"> Index URL</a>'
+                    msg += f'\n\n<b>○ 📁 Index Link:</b> {url}'
             else:
-                msg += f"⁍ <a href='https://drive.google.com/uc?id={file.get('id')}" \
-                       f"&export=download'>{file.get('name')}</a> ({get_readable_file_size(int(file.get('size')))})"
+                msg += f"⁍ https://drive.google.com/uc?id={file.get('id')}" \
+                       f"&export=download' ({get_readable_file_size(int(file.get('size')))})"
                 if INDEX_URL is not None:
                     url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}')
-                    msg += f' | <a href="{url}"> Index URL</a>'
+                    msg += f'\n\n<b>○ 📁 Index Link:</b> {url}'
             msg += '\n'
         return msg
